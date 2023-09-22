14997212c92980013bccd0f215fcb17e843c2c0a
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2023-09-22 11:23:53 +0200

    ok

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Safely store message");
+Console.WriteLine("Read message");
+Console.WriteLine("Exit");
+
+string? enteredText = Console.ReadLine();
+
+ConsoleKeyInfo key = Console.ReadKey(intercept: true);
+//Check if a specific key is pressed
+bool isEnter = key.key == ConsoleKey.Enter;
+//Get the character that was pressed
+bool character = key.keyChar;
+
